4401c0a46abe583a497ff79755047de38de379a0
MarcSogas <marc.sogas@gmail.com>
2022-09-14 18:16:37 +0200

    add toast

--- app/src/main/java/com/marcsogasdev/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/marcsogasdev/myfirstapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +15,14 @@ class MainActivity : AppCompatActivity() {
         val tvMyTextview =  findViewById<TextView>(R.id.textView)
         val tvCountClicker =  findViewById<TextView>(R.id.textViewClick)
         var timesClicked =0
+
         btnClickMe.setOnClickListener{
             btnClickMe.text="HAHA you click me!"
             tvMyTextview.text= "Great, you click de button 👍!"
             timesClicked += 1
             tvCountClicker.text=timesClicked.toString()
+
+            Toast.makeText(this,"Hey I'm a toast!",Toast.LENGTH_SHORT).show()
 
 
         }
