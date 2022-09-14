b345f5d2d6c111f751cd4bc8b99f8bce12107f9a
MarcSogas <marc.sogas@gmail.com>
2022-09-14 18:03:31 +0200

    Change button

--- app/src/main/java/com/marcsogasdev/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/marcsogasdev/myfirstapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.marcsogasdev.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        btnClickMe.setOnClickListener{
+            btnClickMe.text="HAHA you click me!"
+
+        }
     }
 }
