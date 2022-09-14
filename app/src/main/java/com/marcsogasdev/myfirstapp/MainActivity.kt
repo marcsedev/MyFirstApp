8ee4dcb384f6504ebe3e7dd60de183625e0568fd
MarcSogas <marc.sogas@gmail.com>
2022-09-14 18:07:51 +0200

    Change text when click button

--- app/src/main/java/com/marcsogasdev/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/marcsogasdev/myfirstapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.marcsogasdev.myfirstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +11,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val tvMyTextview =  findViewById<TextView>(R.id.textView)
+
         btnClickMe.setOnClickListener{
             btnClickMe.text="HAHA you click me!"
+            tvMyTextview.text= "Great, you click de button 👍!"
 
         }
     }
